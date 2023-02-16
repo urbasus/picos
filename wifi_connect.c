1869423ef54c3c4b20e4ad74bc2eff970bc2d9ce
Andreas Urbán <andreas.urban@hotmail.com>
2023-02-16 11:53:44 +0100

    Add proven wifi connect sequence

--- wifi_connect.c
+++ wifi_connect.c
@@ -7,40 +7,42 @@
 #include "pico/stdlib.h"
 #include "pico/cyw43_arch.h"
 
-const uint32_t blink_pattern_retry_ms = 100;
-const uint32_t blink_pattern_halt_ms = 250;
+const uint32_t blink_pattern_retry_ms = 250;
+const uint32_t blink_pattern_halt_ms = 500;
 const uint32_t blink_pattern_success_ms = 1000;
 
-const uint32_t wifi_timeout_ms = 1000;
+const uint32_t wifi_timeout_ms = 10000;
 
 #ifdef WIFI_CREDENTIALS_FILE
 #include WIFI_CREDENTIALS_FILE
 #endif
 
-void blink(uint32_t ms)
+void blink(uint32_t ms, uint32_t iterations)
 {
-    cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 0);
-    sleep_ms(ms);
-    cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
-    sleep_ms(ms);
+    while (iterations--)
+    {
+        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 0);
+        sleep_ms(ms);
+        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
+        sleep_ms(ms);
+    }
 }
 
 void halt()
 {
     while (true) {
-        blink(blink_pattern_halt_ms);
+        blink(blink_pattern_halt_ms, 1);
     }
 }
 
 void retry()
 {
-    blink(blink_pattern_retry_ms);
-    blink(blink_pattern_retry_ms);
+    blink(blink_pattern_retry_ms, 2);
 }
 
 int main() {
     /* Initialize wifi driver (and LED GPIO) */
-    if (cyw43_arch_init() != 0) {
+    if (cyw43_arch_init_with_country(CYW43_COUNTRY_SWEDEN) != 0) {
         return -1;
     }
 
@@ -49,15 +51,19 @@ int main() {
     /* TODO: Blink pattern in separate thread/core? */
     cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
 
+    /* Enable STAtion mode */
+    cyw43_arch_enable_sta_mode();
+
     /* Connect to wifi */
-    while (cyw43_arch_wifi_connect_timeout_ms(WIFI_SSID, WIFI_PW, CYW43_AUTH_WPA2_AES_PSK, wifi_timeout_ms) != 0)
+    int code;
+    while (code = cyw43_arch_wifi_connect_timeout_ms(WIFI_SSID, WIFI_PW, CYW43_AUTH_WPA2_AES_PSK, wifi_timeout_ms) != 0)
     {
-        retry();
+        blink(blink_pattern_retry_ms, abs(code));
     }
 
     /* Message success */
     while(true)
     {
-        blink(blink_pattern_success_ms);
+        blink(blink_pattern_success_ms, 1);
     }
 }
